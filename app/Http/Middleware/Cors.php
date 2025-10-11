5da70e8e132edff127ce86db19d275ae34e93393
ramizaiter1989 <ramizaiter1989@gmail.com>
2025-10-11 18:49:18 +0200

    railway setting with api's was edited before 1

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -10,13 +10,13 @@ class Cors
 {
     $response = $next($request);
 
-    $response->headers->set('Access-Control-Allow-Origin', 'https://b4d50a19dba0.ngrok-free.app');
+    $response->headers->set('Access-Control-Allow-Origin', 'https://gondola.proxy.rlwy.net');
     $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
     if ($request->getMethod() === "OPTIONS") {
         return response('', 200)
-            ->header('Access-Control-Allow-Origin', 'https://b4d50a19dba0.ngrok-free.app')
+            ->header('Access-Control-Allow-Origin', 'https://gondola.proxy.rlwy.net')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'https://b4d50a19dba0.ngrok-free.app,localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'https://gondola.proxy.rlwy.net,localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort(),
         // Sanctum::currentRequestHost(),
     ))),
