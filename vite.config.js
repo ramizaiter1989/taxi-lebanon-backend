77d66910c7644b458441eb2d1dad8cbe66f66514
ramizaiter1989 <ramizaiter1989@gmail.com>
2025-10-12 06:05:25 +0200

    render api otp and email fix 5 vite solved 1

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   },
   plugins: [
     laravel({
-      input: ['resources/css/app.css', 'resources/js/app.jsx', 'resources/js/dashboard.js'],
+      input: ['resources/css/app.css', 'resources/js/app.jsx', 'resources/js/dashboard.js','resources/js/bootstrap.js'],
       refresh: true,
     }),
   ],
