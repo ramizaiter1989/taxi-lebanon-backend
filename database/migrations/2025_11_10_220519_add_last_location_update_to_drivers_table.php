9b579fc630a325c0d885e1837adce491304afbab
ramizeaiter <raamizaiter1989@gmail.com>
2025-11-10 23:08:20 +0100

    driver location saved correctly

---
+++ database/migrations/2025_11_10_220519_add_last_location_update_to_drivers_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+   public function up(): void
+{
+    Schema::table('drivers', function (Blueprint $table) {
+        $table->timestamp('last_location_update')->nullable()->after('current_driver_lng');
+    });
+}
+
+public function down(): void
+{
+    Schema::table('drivers', function (Blueprint $table) {
+        $table->dropColumn('last_location_update');
+    });
+}
+
+};
